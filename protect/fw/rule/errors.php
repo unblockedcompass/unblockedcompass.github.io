5132a1c3d15ecfce6542586fc4b1c6010d5e7bdf
unblockedcompass <152827867+unblockedcompass@users.noreply.github.com>
2024-11-17 22:15:56 +1100

    Delete protect directory

--- protect/fw/rule/errors.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('ABSPATH') && !defined('MCDATAPATH')) exit;
-
-if (!class_exists('MCProtectRuleError_V581')) :
-class MCProtectRuleError_V581 extends Exception {
-//Root rule error class.
-}
-endif;
